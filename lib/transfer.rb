11057d6f4fb362996c10219a3530ac22c5e02d84
Shanise Barona <shanisebarona@gmail.com>
2018-09-01 02:17:52 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -30,8 +30,8 @@ class Transfer
   
   def reverse_transfer
     @sender.balance += @amount
-      @receiver.balance -= @amount
-      @status = "reversed"
+    @receiver.balance -= @amount
+    @status = "reversed"
   end
   
 end
