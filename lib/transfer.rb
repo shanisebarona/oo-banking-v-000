ed4743ecb4c697109073db83050832076b620113
Shanise Barona <shanisebarona@gmail.com>
2018-09-01 02:14:06 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end
   
   def execute_transaction
-    # executing successful transaction
+    
     @sender.balance -= @amount #@sender.balance = @sender.balance - @amount
     @receiver.balance += @amount
     @status = "complete"
